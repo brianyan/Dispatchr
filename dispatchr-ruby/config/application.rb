4d2464a55b1e93c3d0aa65507c7d43fc8449dd37
spencerprescott <stp@umail.ucsb.edu>
2016-10-17 16:08:36 -0700

    Use rspec and factory girl for tests

--- dispatchr-ruby/config/application.rb
+++ dispatchr-ruby/config/application.rb
@@ -26,5 +26,10 @@ module DispatchrRuby
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl
+      g.factory_girl dir: 'spec/factories'
+    end
   end
 end
