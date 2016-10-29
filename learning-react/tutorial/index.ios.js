b0c6ef2658730389a054e65171dddc368b57ade6
Brian Yan <score14@gmail.com>
2016-10-29 16:42:36 -0700

    Cleaned up tutorial code

--- learning-react/tutorial/index.ios.js
+++ learning-react/tutorial/index.ios.js
@@ -1,18 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry,View, Text, Image } from 'react-native';
 
-class tutorial extends Component {
-  render() {
-    return (
-      <Text>Hello world!</Text>
-    );
-  }
+class Greeting extends Component {
+	render() {
+		const {name} = this.props; // creates local variable name that is set to this.props.name
+		const imageURLS = {
+			"Brian" : "https://coderssb.com/home/_officers/Brian.jpg",
+			"Alok"  : "https://coderssb.com/home/_officers/Alok.jpg",
+			"Jordan" : "https://coderssb.com/home/_officers/Jordan.jpg"
+		};
+		const urlToShow = imageURLS[name];
+		return (
+
+			<View style = {{alignItems: 'center'}}>
+				<Text>Hello {this.props.name}!</Text>
+				<Text>Hello {urlToShow}!</Text>
+				<Image source = {{uri : 'https://coderssb.com/home/_officers/Jordan.jpg' }} />
+				<Image source= {urlToShow}/>
+			</View>
+			);
+	}
 }
 
-AppRegistry.registerComponent('tutorial', () => tutorial);
+class LotsOfGreetings extends Component {
+	render() {
+
+		return (
+			<View style = {{alignItems: 'center'}}>
+				<Greeting name = 'Alok' />
+				<Greeting name = 'Jordan'/>
+				<Greeting name = 'Brian'/>
+			</View>
+		);
+	}
+}
+
+AppRegistry.registerComponent('tutorial', () => LotsOfGreetings);
