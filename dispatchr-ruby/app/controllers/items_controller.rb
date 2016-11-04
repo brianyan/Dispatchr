42d99fb40239e2385acd6391ee9ad443e6bb8599
Alok <alokg@me.com>
2016-11-03 21:25:23 -0700

    CRUD endpoints for items

--- dispatchr-ruby/app/controllers/items_controller.rb
+++ dispatchr-ruby/app/controllers/items_controller.rb
@@ -4,13 +4,63 @@ class ItemsController < ApplicationController
   end
 
   def show
+    if params[:id].present?
+      item_id = params[:id]
+      item = Item.find(item_id)
 
+      if item.present?
+        render json: item
+      else
+        render json: item.errors, status: :unprocessable_entity
+      end
+
+    else
+      error_str = "No/Invalid item ID entered"
+      render json: error_str, status: :unprocessable_entity
+    end
   end
 
   def create
-    item_name = params[:name]
-    item = Item.create(name: item_name)
-    render json: item
+    if params[:name].present?
+      item_name = params[:name]
+      item = Item.new(name: item_name)
+
+      if item.save
+        render json: item
+      else
+        render json: item.errors, status: :unprocessable_entity
+      end
+
+    else
+        error_str = 'No item name entered or name is invalid. Please try a valid name and try again'
+        render json: error_str, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if params[:id].present? && params[:name].present?
+      item = Item.find(params[:id])
+      item.name = params[:name]
+      if item.save
+        render json: item
+      else
+        error_str = 'No item name entered or name is invalid. Please try a valid name and try again'
+        render json: error_str, status: :unprocessable_entity
+      end
+    else
+      error_str = 'No item name entered or name is invalid. Please try a valid name and try again'
+      render json: error_str, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if params[:id].present?
+      item = Item.find(params[:id])
+      render status: 200, json: item.destroy
+    else
+      error_str = 'Invalid item ID. Try again.'
+      render json: error_str, status: :unprocessable_entity
+    end
   end
 
 
