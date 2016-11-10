ee9a9a242ff1ca5127764ff5516ec94ae113934a
spencerprescott <stp@umail.ucsb.edu>
2016-11-09 20:27:13 -0800

    Add basic list view for request items

--- Dispatchr/app/request_items/component.js
+++ Dispatchr/app/request_items/component.js
@@ -1,23 +1,67 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { View, Text, ListView, TouchableHighlight, StyleSheet } from 'react-native';
 import * as requestsActions from './actions';
 import { connect } from 'react-redux';
 
-class RequestsList extends Component {
+class RequestItemsList extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds,
+    };
+
+    this.props.getRequestItems();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(nextProps.request_items)
+    })
+  }
+
   render() {
     return (
-      <TouchableOpacity onPress={this.props.getRequestItems}>
-        <Text>Hi</Text>
-        <Text>{JSON.stringify(this.props)}</Text>
-      </TouchableOpacity>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+      />
+    );
+  }
+
+  _renderRow(rowData, sectionId, rowId, highlightRow) {
+    const rowAction = () => {
+      highlightRow(sectionId, rowId);
+    };
+    return (
+      <TouchableHighlight onPress={rowAction}>
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.text}>
+              {rowData}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
     );
   }
 }
 
-export default connect(state => ({
-    requests: state.requestItemsReducer
-  }),
+var styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#F6F6F6',
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+
+export default connect(state => (state.requestItemsReducer),
   {
     getRequestItems: requestsActions.getRequestItems,
   }
-)(RequestsList);
+)(RequestItemsList);
