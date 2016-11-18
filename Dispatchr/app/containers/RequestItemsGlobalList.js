3cdd0f25f7d1fdeaa7ace8f6c281c9b5430acbc7
Sal Olivares <git@salolivares.com>
2016-11-18 00:47:51 -0800

    changed colors of button

--- Dispatchr/app/containers/RequestItemsGlobalList.js
+++ Dispatchr/app/containers/RequestItemsGlobalList.js
@@ -62,15 +62,9 @@ class RequestItemsGlobalList extends Component {
         />
         )}
 
-        <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+        <ActionButton buttonColor="#0288D1">
+          <ActionButton.Item buttonColor='#00897B' title="New Request" onPress={() => console.log("New Requested Tapped")}>
             <Icon name="md-create" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-            <Icon name="md-done-all" style={styles.actionButtonIcon} />
           </ActionButton.Item>
         </ActionButton>
     </View>
