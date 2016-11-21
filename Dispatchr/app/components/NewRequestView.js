6ea5cceff49a92581fedc1feb748dbd579506f8e
Sal Olivares <git@salolivares.com>
2016-11-21 00:25:05 -0800

    Trying to use non-saga method
    Right now, saga is proving to not work with the popup dialog. Everything worked when it was all in one file, but thats not clean at all. So as of now, I still can't figure out how to cleanly implement it.

--- Dispatchr/app/components/NewRequestView.js
+++ Dispatchr/app/components/NewRequestView.js
@@ -6,37 +6,46 @@ import { Text, View, StyleSheet} from 'react-native';
 import { connect } from 'react-redux';
 import { ActionCreators } from '../actions';
 import { bindActionCreators } from 'redux';
+import ActionButton from 'react-native-action-button';
 
-class NewRequestView extends Component {
+
+export default class NewRequestView extends Component {
   render() {
     return (
-      <PopupDialog
-          ref={(popupDialog) => { this.popupDialog = popupDialog; }}
-          dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' }) }
-          dialogTitle={<DialogTitle title="New Request" />}
-          width={340}
-          height={450}
-          overlayOpacity={0.75}
-          open={this.props.showPopupDialog}
-        >
-          <View>
-              <TextField label={'Name'} highlightColor={'#00BCD4'} />
-              <TextField
-                label={'Qty.'}
-                highlightColor={'#00BCD4'}
-                keyboardType={'numeric'}
-              />
-              <Button style={styles.requestButtonSave} onPress={()=>this._saveRequestAndAddNewItem()}textStyle={{color: 'white'}}>
-                Save + Add
-              </Button>
-              <Button style={styles.requestButtonSave} onPress={() => this._saveNewRequest()} textStyle={{color: 'white'}}>
-                Save
-              </Button>
-          </View>
-        </PopupDialog>
+      <View>
+        <PopupDialog
+            ref={(popupDialog) => { this.popupDialog = popupDialog; }}
+            dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' }) }
+            dialogTitle={<DialogTitle title="New Request" />}
+            width={340}
+            height={450}
+            overlayOpacity={0.75}
+          >
+            <View>
+                <TextField label={'Name'} highlightColor={'#00BCD4'} />
+                <TextField
+                  label={'Qty.'}
+                  highlightColor={'#00BCD4'}
+                  keyboardType={'numeric'}
+                />
+                <Button style={styles.requestButtonSave} onPress={()=>this._saveRequestAndAddNewItem()}textStyle={{color: 'white'}}>
+                  Save + Add
+                </Button>
+                <Button style={styles.requestButtonSave} onPress={() => this._saveNewRequest()} textStyle={{color: 'white'}}>
+                  Save
+                </Button>
+            </View>
+          </PopupDialog>
+
+          <ActionButton buttonColor="#0288D1" onPress={() => this._showPopup()} />
+        </View>
     );
   }
 
+  _showPopup = () => {
+    console.log('hit');
+    this.popupDialog.openDialog();
+  }
   _saveNewRequest = () => {
     console.log("Request Saved!");
   }
@@ -55,18 +64,4 @@ const styles = StyleSheet.create({
     height: 25
   },
 });
-
-
-/* Connects to the actions, so we can do stuff! Boilerplate!!! */
-function mapDispatchToProps(dispatch){
-  return bindActionCreators(ActionCreators, dispatch);
-}
-
-function mapStateToProps(state) {
-  return {
-      showPopupDialog: state.showPopupDialog
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(NewRequestView);
 
--- Dispatchr/app/containers/RequestItemsGlobalList.js
+++ Dispatchr/app/containers/RequestItemsGlobalList.js
@@ -4,9 +4,8 @@ import { ActionCreators } from '../actions';
 import { bindActionCreators } from 'redux';
 import renderIf from '../lib/renderif'
 import { Actions } from 'react-native-router-flux'
-import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
-import NewRequestView from '../components/NewRequestView.js';
+import NewRequestView from '../components/NewRequestView';
 
 import {
   View,
@@ -28,7 +27,6 @@ class RequestItemsGlobalList extends Component {
       refreshing: false,
       dataSource: ds,
       showFetchButton: true,
-      showNewRequestButton: true
     };
     this.props.getRequestItems();
   }
@@ -58,7 +56,6 @@ class RequestItemsGlobalList extends Component {
               refreshing={this.state.refreshing}
               onRefresh={this._onRefresh.bind(this)}
             />}
-          onScroll={this._onScroll}
           dataSource={this.state.dataSource}
           renderRow={this._renderRow}
           renderSeparator={this._renderSeparator}
@@ -69,7 +66,6 @@ class RequestItemsGlobalList extends Component {
 
         <NewRequestView />
 
-        {this.state.showNewRequestButton ? <ActionButton buttonColor="#0288D1" onPress={() => this.props.newRequest()} /> : null}
     </View>
     );
   }
@@ -104,33 +100,6 @@ class RequestItemsGlobalList extends Component {
     return (
       <View key={rowId} style={styles.separator} />
     );
-  }
-
-  _onScroll = (event) => {
-    // Simple fade-in / fade-out animation
-    const CustomLayoutLinear = {
-      duration: 100,
-      create: { type: LayoutAnimation.Types.linear, property: LayoutAnimation.Properties.opacity },
-      update: { type: LayoutAnimation.Types.linear, property: LayoutAnimation.Properties.opacity },
-      delete: { type: LayoutAnimation.Types.linear, property: LayoutAnimation.Properties.opacity }
-    }
-
-    // Check if the user is scrolling up or down by confronting the new scroll position with your own one
-    const currentOffset = event.nativeEvent.contentOffset.y
-
-    const direction = (currentOffset > 0 && currentOffset > this._listViewOffset)
-      ? 'down'
-      : 'up'
-
-    // If the user is scrolling down (and the action-button is still visible) hide it
-    const showNewRequestButton = direction === 'up'
-
-    if (showNewRequestButton !== this.state.showNewRequestButton) {
-      LayoutAnimation.configureNext(CustomLayoutLinear)
-      this.setState({ showNewRequestButton })
-    }
-    // Update scroll position
-    this._listViewOffset = currentOffset
   }
 
   _renderFooter(){
