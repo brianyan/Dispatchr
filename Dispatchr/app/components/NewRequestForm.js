9ce4c136aaf39a6222a2fb45eced2d4a9fc32f64
Sal Olivares <git@salolivares.com>
2016-11-22 01:59:46 -0800

    partial integrated redux forms

---
+++ Dispatchr/app/components/NewRequestForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import TextField from 'react-native-md-textinput';
+import { Text, TextInput,  View, StyleSheet, TouchableHighlight} from 'react-native';
+
+class ItemNameInput extends Component {
+  render() {
+    return (
+      <View>
+        <TextField label={'Name'} highlightColor={'#00BCD4'} />
+      </View>
+    )
+  }
+}
+
+class QuantityInput extends Component {
+  render() {
+    return (
+      <View>
+        <TextField
+            label={'Qty.'}
+            highlightColor={'#00BCD4'}
+            keyboardType={'numeric'}
+        />
+      </View>
+    )
+  }
+}
+
+class NewRequestForm extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <View>
+          <Field name="ItemName" component={ItemNameInput}  />
+          <Field name="Quantity" component={QuantityInput}  />
+      </View>
+    );
+  }
+}
+
+// Decorate the form component
+NewRequestForm = reduxForm({
+  form: 'newRequest' // a unique name for this form
+})(NewRequestForm);
+
+export default NewRequestForm;
--- Dispatchr/app/components/NewRequestView.js
+++ Dispatchr/app/components/NewRequestView.js
@@ -2,50 +2,16 @@ import React, { Component } from 'react';
 import TextField from 'react-native-md-textinput';
 import Button from 'apsl-react-native-button';
 import { Text, View, StyleSheet} from 'react-native';
-import { Field, reduxForm } from 'redux-form';
-
-
+import NewRequestForm from './NewRequestForm'
 
 export default class NewRequestView extends Component {
+  handleSubmit = (values) => {
+    console.log(values);
+  }
   render() {
     return (
-      <View>
-          <TextField label={'Name'} highlightColor={'#00BCD4'} />
-          <TextField
-            label={'Qty.'}
-            highlightColor={'#00BCD4'}
-            keyboardType={'numeric'}
-          />
-          <Button style={styles.requestButtonSave} onPress={()=>this._saveRequestAndAddNewItem()}textStyle={{color: 'white'}}>
-            Save + Add
-          </Button>
-          <Button style={styles.requestButtonSave} onPress={() => this._saveNewRequest()} textStyle={{color: 'white'}}>
-            Save
-          </Button>
-      </View>
+      <NewRequestForm onSubmit={this.handleSubmit} />
     );
   }
-
-  _showPopup = () => {
-    console.log('hit');
-    this.popupDialog.openDialog();
-  }
-  _saveNewRequest = () => {
-    console.log("Request Saved!");
-  }
-
-  _saveRequestAndAddNewItem = () => {
-    console.log("Added new item, and saved request");
-  }
-
 }
-
-const styles = StyleSheet.create({
-  requestButtonSave: {
-    borderColor: '#2980b9',
-    backgroundColor: '#3498db',
-    width: 100,
-    height: 25
-  },
-});
 
