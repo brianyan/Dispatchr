f716ba6e1fe6e038a5162d78719f825c80c6356b
Sal Olivares <git@salolivares.com>
2016-11-22 23:01:59 -0800

    added need by button

--- Dispatchr/app/components/NewRequestView.js
+++ Dispatchr/app/components/NewRequestView.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import TextField from 'react-native-md-textinput';
-import Button from 'apsl-react-native-button';
-import { Text, View, StyleSheet} from 'react-native';
-import NewRequestForm from './NewRequestForm'
+import { Text, View, StyleSheet, TouchableHighlight} from 'react-native';
 
 export default class NewRequestView extends Component {
-  handleSubmit = (values) => {
-    console.log(values);
-  }
   render() {
     return (
-      <NewRequestForm onSubmit={this.handleSubmit} />
+      <View style={{flex: 1}}>
+          <TouchableHighlight style={styles.buttonWrapper} onPress = {() =>  { console.log("Needed by...") } }>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Date Needed</Text>
+              <Text style={styles.buttonText}>11/2/16</Text>
+            </View>
+          </TouchableHighlight>
+      </View>
     );
   }
 }
 
+var styles = StyleSheet.create({
+  buttonWrapper: {
+    height: 50, 
+    borderBottomColor: '#47315a', 
+    borderBottomWidth: 1,
+  },
+  button: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingHorizontal: 20,
+      paddingVertical: 15,
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
