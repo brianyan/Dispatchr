04a9c352522d385725ae4ca40b4ac1b0942b61d2
Sal Olivares <git@salolivares.com>
2016-11-22 23:35:38 -0800

    added date picker

--- Dispatchr/app/components/NewRequestView.js
+++ Dispatchr/app/components/NewRequestView.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, TouchableHighlight} from 'react-native';
+import DatePicker from 'react-native-datepicker';
 
 export default class NewRequestView extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-          <TouchableHighlight style={styles.buttonWrapper} onPress = {() =>  { console.log("Needed by...") } }>
-            <View style={styles.neededByButton}>
-              <Text style={styles.buttonText}>Date Needed</Text>
-              <Text style={styles.buttonText}>11/2/16</Text>
-            </View>
-          </TouchableHighlight>
+          <View style={styles.expirationButton}>
+            <Text style={{fontSize: 16}}>Date Needed</Text>
+            <DatePicker
+              style={{width: 200}}
+              date={'2016-05-01'}
+              mode="date"
+              placeholder="placeholder"
+              format="YYYY-MM-DD"
+              minDate="2016-05-01"
+              maxDate="2016-06-01"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              onDateChange={(date) => {console.log('data changed')}}
+            />
+          </View>
           <Text style={{flex: 1}}> Hello! </Text>
           <TouchableHighlight style={styles.publishRequestButton} onPress = {() =>  { console.log("Needed by...") } }>
             <Text style={styles.buttonText}>Publish my Request</Text>
@@ -21,22 +31,17 @@ export default class NewRequestView extends Component {
 }
 
 var styles = StyleSheet.create({
-  buttonWrapper: {
+  expirationButton: {
     height: 50, 
     borderBottomWidth: 1,
-  },
-  neededByButton: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      paddingHorizontal: 20,
-      paddingVertical: 15,
-  },
-  buttonText: {
-    fontSize: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
   },
   publishRequestButton: {
     flex: 0,
-    color: 'orange',
+    backgroundColor : '#77c2e5',
     justifyContent: 'center',
     alignItems: 'center',
     borderTopWidth: 1,
