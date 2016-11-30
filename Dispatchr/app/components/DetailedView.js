60917068a7a0a3b7dc3ee39a706d93b59ee0d90f
Brian Yan <score14@gmail.com>
2016-11-29 21:05:52 -0800

    refactored DetailedView

--- Dispatchr/app/components/DetailedView.js
+++ Dispatchr/app/components/DetailedView.js
@@ -1,28 +1,108 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, Alert, TouchableHighlight} from 'react-native';
+import { Actions} from 'react-native-router-flux';
+var MapView = require('react-native-maps');
+
 
 export default class DetailedView extends Component {
+  _alertAccept() {
+    Alert.alert(
+      'Request Accepted',
+      "You're a hero!",
+      [
+        {text: 'OK', onPress: () => {Actions.pop()}},
+      ]
+    )
+  }
+  _alertHide() {
+    Alert.alert(
+      'Request Declined',
+      "You'll get them next time!",
+      [
+        {text: 'OK', onPress: () => {Actions.pop()}},
+      ]
+    )
+  }
   render() {
+
     return (
-      <View>
-        <Text style = {styles.red}>
-          {this.props.rowData.id}
-        </Text>
-        <Text style = {styles.red}>
-          {this.props.rowData.name}
-        </Text>
+      <View style = {{flex: 1}}>
+        <View style = {styles.attributeWrapper}>
+          <Text style = {styles.attributeText}>
+            {this.props.rowData.id}
+          </Text>
+          <Text style = {styles.attributeText}>
+            {this.props.rowData.name}
+          </Text>
+        </View>
+        <View style={styles.content}>
+          <TouchableHighlight style={{flex: 1, alignItems: 'center'}} onPress = {() => { this._alertAccept() }}>
+            <Text> Accept </Text>
+          </TouchableHighlight>
+          <View style={styles.divider}></View>
+          <TouchableHighlight style={{flex: 1, alignItems: 'center'}} onPress = {() => { this._alertHide() } }>
+            <Text> Hide </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
+    content: {
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        borderColor: '#EAEAEA',
+        borderWidth: 1,
+    },
+    buttonText: {
+        fontSize: 20
+    },
+    divider: {
+      backgroundColor: '#EAEAEA',
+      width: 1,
+      height: 35,
+      marginTop: 5,
+      marginBottom: 5
+    }
 });
+
+// var styles = StyleSheet.create({
+//   HideButton: {
+//     backgroundColor : 'red',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderTopWidth: 1,
+//     height: 50,
+//     flex: 0
+//   },
+//   AcceptButton: {
+//     backgroundColor : 'green',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderTopWidth: 1,
+//     height: 50,
+//     flex: 0
+//   },
+//   buttonWrapper: {
+//     flexDirection: 'row',
+//     flex: 0,
+//   },
+//   attributeWrapper: {
+//     flexDirection: 'row',
+//     flex: 1,
+//   },
+//   attributeText: {
+//     fontSize: 18,
+//     fontFamily: "Helvetica Neue",
+//     color: '#48BBEC',
+//     alignSelf: 'center',
+//     marginTop: 10,
+//   },
+//   buttonText:{
+//     color: 'white'
+//   },
+// });
