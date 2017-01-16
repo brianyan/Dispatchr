cffbff1cb6075d779be5fae7c4787a45e050984b
Brian Yan <score14@gmail.com>
2017-01-16 15:04:32 -0800

    started on chat_service

---
+++ Dispatchr/app/containers/Chat.js
@@ -0,0 +1,9 @@
+import { GiftedChat } from 'react-native-gifted-chat';
+const Firebase = require('Firebase');
+
+class ChatContainer extends Component {
+  constructor(props){
+    super(props);
+    this._messageRef = New FireB
+  }
+}
