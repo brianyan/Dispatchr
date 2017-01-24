372a148f2ea2044faab1216f8c45c275afc4fd79
Sal Olivares <git@salolivares.com>
2017-01-23 21:15:17 -0800

    chat ui integrated

--- Dispatchr/app/containers/Chat.js
+++ Dispatchr/app/containers/Chat.js
@@ -1,46 +1,85 @@
 import { GiftedChat } from 'react-native-gifted-chat';
 import React, { Component } from 'react';
+import SocketIOClient from 'socket.io-client';
+
+const USER_ID = '@userId';
 
 export default class ChatView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
+      messages: [],
+      userId: null
     };
+
+    this.determineUser = this.determineUser.bind(this);
+    this.onReceivedMessage = this.onReceivedMessage.bind(this);
     this.onSend = this.onSend.bind(this);
+    this._storeMessages = this._storeMessages.bind(this);
+
+    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket.on('message', this.onReceivedMessage);
+    this.determineUser();
   }
-  componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://facebook.github.io/react/img/logo_og.png',
-          },
-        },
-      ],
-    });
+
+  /**
+   * When a user joins the chatroom, check if they are an existing user.
+   * If they aren't, then ask the server for a userId.
+   * Set the userId to the component's state.
+   */
+  determineUser() {
+    AsyncStorage.getItem(USER_ID)
+      .then((userId) => {
+        // If there isn't a stored userId, then fetch one from the server.
+        if (!userId) {
+          this.socket.emit('userJoined', null);
+          this.socket.on('userJoined', (userId) => {
+            AsyncStorage.setItem(USER_ID, userId);
+            this.setState({ userId });
+          });
+        } else {
+          this.socket.emit('userJoined', userId);
+          this.setState({ userId });
+        }
+      })
+      .catch((e) => alert(e));
   }
-  onSend(messages = []) {
+
+  // Event listeners
+  /**
+   * When the server sends a message to this.
+   */
+  onReceivedMessage(messages) {
+    this._storeMessages(messages);
+  }
+
+  /**
+   * When a message is sent, send the message to the server
+   * and store it in this component's state.
+   */
+  onSend(messages=[]) {
+    this.socket.emit('message', messages[0]);
+    this._storeMessages(messages);
+  }
+
+  render() {
+    var user = { _id: this.state.userId || -1 };
+
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={this.onSend}
+        user={user}
+      />
+    );
+  }
+
+  // Helper functions
+  _storeMessages(messages) {
     this.setState((previousState) => {
       return {
         messages: GiftedChat.append(previousState.messages, messages),
       };
     });
-  }
-  render() {
-    return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={this.onSend}
-        user={{
-          _id: 1,
-        }}
-      />
-    );
   }
 }
