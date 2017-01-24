3b5c28b2118b022108f0f96534258df7ebbd47aa
Brian Yan <score14@gmail.com>
2017-01-23 19:27:17 -0800

    chatserver in progress

--- chat_service/database/schemas/message.js
+++ chat_service/database/schemas/message.js
@@ -2,11 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var messageSchema = new Schema({
-  messageBody : String,
+  users: {id1: String, id2: String},
+  user_msgs : [{is_sender: String, msg_body: String, timestamp: {type: Date, default: Date.now}}],
   received: Boolean,
-  sender: String,
-  receiver: String,
-  date : {type: Date, default: Date.now},
 })
 
 var Message = mongoose.model('Message', messageSchema);
--- chat_service/server.js
+++ chat_service/server.js
@@ -9,20 +9,16 @@ var websocket = socketio(server);
 // database stuff
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://user:password@ds117109.mlab.com:17109/dispatchrchat');
-
+var Message = require('./database/schemas/message');
 app.get('/', function(req, res){
-  // res.send('ok');
-  var Message = require('./database/schemas/message');
+
   var sample = new Message({
-    messageBody : "Hello there",
+    users: {id1:"Alok", id2:"Spencer"},
+    user_msgs : {is_sender: "Alok", msg_body: "testing", timestamp: new Date()},
     received: false,
-    sender: "Brian",
-    receiver: "Melinda",
-    date : new Date(),
   });
 
   console.log("hello");
-
 
   sample.save(function(err, sample){
     if(err) {
@@ -34,31 +30,33 @@ app.get('/', function(req, res){
   })
 });
 
-// app.get('/messages', func)
+app.get('/chatroom', function(req, res) {
+  Message.findOne({users: {id1:'Alok', id2: 'Spencer'}}, function(err, message){
+    console.log(message);
+    message.user_msgs.push({is_sender: "Alok", msg_body: "testing2", timestamp: new Date()});
+    message.save(function(err) {
+      if(err) {
+        console.log(message);
+      }
+    });
+  });
+});
+// app.get('/sendMessage', function())
+
+// function _sendAndSaveMessage(message, socket, fromServer) {
+//   var messageData = {
+//     text: message.text,
+//     user: message.user,
+//     createdAt: new Date(message.createdAt),
+//     chatId: chatId
+//   };
 //
-// app.get('/', function(req, res){
-//   var Message = require('./database/schemas/message');
-//   var sample = new Message({
-//     messageBody : "Hello there",
-//     received: false,
-//     sender: "Brian",
-//     receiver: "Melinda",
-//     date : Date.now,
+//   db.collection('messages').insert(messageData, (err, message) => {
+//     // If the message is from the server, then send to everyone.
+//     var emitter = fromServer ? websocket : socket.broadcast;
+//     emitter.emit('message', [message]);
 //   });
-//
-//   console.log("hello");
-//
-//
-//   sample.save(function(err, sample){
-//     if(err) {
-//       return console.error(err);
-//     } else {
-//       res.send(sample);
-//     }
-//     console.log("Message saved successfully");
-//   })
-// });
-//
+// }
 server.listen(3000, () => console.log('listening on *:3000'));
 
 // websocket.on('connection', (socket) => {
