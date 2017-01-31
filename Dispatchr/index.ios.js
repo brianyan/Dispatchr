04bd749ff6cbffb1ad7e0649d4d3a956bfabb4f6
Sal Olivares <git@salolivares.com>
2017-01-30 16:23:51 -0800

    added firebase js integration

--- Dispatchr/index.ios.js
+++ Dispatchr/index.ios.js
@@ -4,7 +4,11 @@ import {
 } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './configureStore';
+import { firebaseConfig } from './config.firebase'
 import App from './app/containers/App';
+import * as firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class Dispatchr extends Component {
   render() {
