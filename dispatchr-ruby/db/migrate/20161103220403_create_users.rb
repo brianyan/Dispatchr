1e39ce32e20e6044f6f1057d4c692db3f08e650a
Alok <alokg@me.com>
2017-02-07 14:32:00 -0800

    user pass fixes

--- dispatchr-ruby/db/migrate/20161103220403_create_users.rb
+++ dispatchr-ruby/db/migrate/20161103220403_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :username
-      t.string :password_digest
       t.string :email
       t.belongs_to :address, index: true
       t.timestamps
