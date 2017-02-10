0b60d019bcc54522a3a3761d217ee62fa8aa5b09
Jordan Nguyen <jordannguyen95@gmail.com>
2017-02-09 18:01:36 -0800

    fixed routing issue.

--- dispatchr-ruby/config/routes.rb
+++ dispatchr-ruby/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :items, :requests, :users, :request_items, :addresses, :notifications
-  get '/users/username/', to: 'users#show_by_email'
-  get '/users/email/', to: 'users#show_by_username'
+  get '/users/username/', to: 'users#show_by_username'
+  get '/users/email/', to: 'users#show_by_email'
   get '/requests/user/:id' => 'requests#search_user'
   get '/request_items/request/' => 'request_items#search_request'
   post '/users' => 'users#create'
@@ -12,4 +11,5 @@ Rails.application.routes.draw do
   post '/requests/complete/:id' => 'requests#complete_request'
   post '/users/reputation/:id/:score' => 'users#update_reputation'
   get '/notifications/get/:user_id' => 'notifications#get_notifications'
+  resources :items, :requests, :users, :request_items, :addresses, :notifications
 end
